8b1a302751de953a91f7b0c04099d3f37c5825eb
Master Of None <nezar.ras@gmail.com>
2021-12-24 15:35:45 +0200

    The Parser Class

---
+++ java/com/craftinginterpreters/lox/Parser.java
@@ -0,0 +1,31 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+
+import static com.craftinginterpreters.lox.TokenType.*;
+
+public class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    private Expr expression() {
+        return equality();
+    }
+
+    private Expr equality() {
+        Expr expr = comparison();
+
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+
+            Token operator = previous();
+             Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+    return expr;    
+}
+    
+}
